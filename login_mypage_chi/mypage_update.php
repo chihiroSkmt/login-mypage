a0ebaa95cfccd0d56bac1fd16dec5c7387de00fc
chihiroSkmt <mosamosa_5k3c_chi@icloud.com>
2020-09-29 20:08:36 +0900

    ログインマイページ

---
+++ login_mypage_chi/mypage_update.php
@@ -0,0 +1,46 @@
+<?php
+mb_internal_encoding("utf8");
+session_start();
+
+try {
+    
+    $pdo = new PDO("mysql:dbname=chiriro;host=localhost;","root","root");
+} catch(PDOException $e) {
+    die("<p>申し訳ございません。現在サーバーが混み合っており一時的にアクセスが出来ません。<br>しばらくしてから再度ログインをしてください。</p>
+        <a href='http://localhost/login_mypage/login.php'>ログイン画面へ</a>"
+       );
+}
+
+
+
+$stmt = $pdo->prepare("update login_mypage set name=?, mail=?, password=?, comments=? where id=?;");
+
+
+$stmt->binbdValue(1,$_POST['name']);
+$stmt->binbdValue(2,$_POST['maile']);
+$stmt->binbdValue(3,$_POST['password']);
+$stmt->binbdValue(4,$_POST['comments']);
+$stmt->binbdValue(5,$_POST['id']);
+
+$stmt->execute();
+
+$stmt = $pdo->prepare("select * from login_mypage where mail = ? && password = ?");
+
+$stmt->bindValue(1,$_POST['mail']);
+$stmt->bindValue(2,$_POST['password]');
+                          
+$stmt->execute();
+
+while($row=$stmt->fetch()) {
+    $_SESSION['id']=$row['id'];
+    $_SESSION['name']=$row['name'];
+    $_SESSION['mail']=$row['mail'];
+    $_SESSION['password']=$row['password'];
+    $_SESSION['picture']=$row['picture'];
+    $_SESSION['comments']=$row['comments'];
+}
+$stmt = NULL; 
+header("Location:mypage.php");
+
+
+?>
